99dd1ea987deacb20bc0b7f590b30586ead0757d
pkiop <vivezdv@gmail.com>
2019-04-23 21:15:07 +0900

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '80'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
